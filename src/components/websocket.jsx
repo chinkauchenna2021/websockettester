9aebfd6990d84aa9fec4383c61b688ee6c4e9d36
chinkauchenna2021 <uchennachinka4@gmail.com>
2024-03-24 03:58:37 -0700

    fixed websocket error

--- src/components/websocket.jsx
+++ src/components/websocket.jsx
@@ -1,75 +1,79 @@
-import  { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-import useLocalStorage from 'use-local-storage';
+import { useState } from "react";
+import io from "socket.io-client";
+import useLocalStorage from "use-local-storage";
 
 const WebSocketClient = () => {
-  const [socketUrl, setSocketUrl] = useState('');
-  const [message, setMessage] = useState('');
-  const [payload, setPayload] = useState('');
-  const [response, setResponse] = useState('');
-  const [socket, setSocket] = useLocalStorage('socket', '');
-
-
-  useEffect(()=>{
-    setSocket("")
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-
-  useEffect(() => {
-
-    if (!socketUrl) return;
-
-          const newSocket = io(socketUrl); // Use socketUrl instead of socket
-          setSocket(newSocket);
-      
-          newSocket.on('connect', () => {
-            console.log('Connected to WebSocket server');
-          });
-      
-          newSocket.on('disconnect', () => {
-            console.log('Disconnected from WebSocket server');
-          });
-      
-          newSocket.on("response", (data) => {
-            console.log("this is the data gotten from Nestjs" , data)
-            setResponse(data);
-          });
-          return () => {
-            newSocket.disconnect();
-          };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [socketUrl]); // Only observe socketUrl
+  const [socketUrl, setSocketUrl] = useState("");
+  const [message, setMessage] = useState("");
+  const [payload, setPayload] = useState("");
+  const [response, setResponse] = useState("");
+  const [socket, setSocket] = useLocalStorage("socket", "");
 
   const connectWebSocket = () => {
     if (!socketUrl) return;
-     setSocketUrl(socketUrl)
+
+    const newSocket = io(socketUrl);
+    setSocket(newSocket);
+    newSocket.on("connect", () => {
+      console.log("Connected to WebSocket server");
+      setResponse("Connected to WebSocket server");
+    });
+
+    newSocket.on("disconnect", () => {
+      console.log("Disconnected from WebSocket server");
+      setSocket(null);
+    });
+
+    newSocket.on("response", (newData) => {
+      console.log("Received response from NestJS:", newData);
+      setResponse(
+        (data) =>
+          data +
+          `\n\r Received response from websocket endpoint :, ${JSON.stringify(
+            newData
+          )} `
+      );
+    });
+
+    return () => {
+      newSocket.disconnect();
+    };
   };
 
   const sendToWebSocket = () => {
-    const newSocket = io(socketUrl); // Use socketUrl instead of socket
-    newSocket.emit(message, payload);
-    console.log('Sent message:', message);
-    setSocket(newSocket)
-      
+    if (!socket || !message || !payload) return;
+
+    socket.emit(message, payload);
+    setSocket(socket);
+    console.log("Sent message:", message);
+    setResponse((data) => data + `\n\r Sent message : ${message} `);
   };
 
   return (
     <div className="container w-full flex justify-center items-center space-y-4 min-h-screen bg-gray-100">
       <div className="max-w-md w-full bg-white shadow-lg rounded-lg overflow-hidden">
         <div className="p-4">
-          <h2 className="text-2xl font-semibold text-center text-blue-400 tracking-wide mb-4">WebSocket Tester</h2>
+          <h2 className="text-2xl font-semibold text-center text-blue-400 tracking-wide mb-4">
+            WebSocket Tester
+          </h2>
           <div className="flex space-x-4">
             <input
               type="text"
+              value={socketUrl}
               onChange={(e) => setSocketUrl(e.target.value)}
               className="flex-1 border border-gray-300 rounded px-4 py-2 focus:outline-none"
               placeholder="wss://example.com"
             />
-            <button onClick={connectWebSocket} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">Connect</button>
+            <button
+              onClick={connectWebSocket}
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+            >
+              Connect
+            </button>
           </div>
           <div className="mt-4">
             <textarea
-              type="text"
+              value={message}
               onChange={(e) => setMessage(e.target.value)}
               className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none"
               placeholder="Enter Message"
@@ -77,19 +81,29 @@ const WebSocketClient = () => {
           </div>
           <div className="mt-4">
             <textarea
-              type="text"
+              value={payload}
               onChange={(e) => setPayload(e.target.value)}
               className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none"
               placeholder="Enter Payload"
             />
           </div>
           <div className="mt-4">
-            <button onClick={sendToWebSocket} className="w-full px-4 py-2 bg-orange-400 text-white rounded hover:bg-orange-500 focus:outline-none">Send</button>
+            <button
+              onClick={sendToWebSocket}
+              className="w-full px-4 py-2 bg-orange-400 text-white rounded hover:bg-orange-500 focus:outline-none"
+            >
+              Send
+            </button>
           </div>
         </div>
         <div className="p-4 bg-gray-200">
+           <div className="text-sm text-red-400 py-2 flex justify-center items-center capitalize">Make sure that your WebSocket emits the message as 'response' with data if any, to allow the tester to capture it </div>
           <h3 className="text-xl font-semibold text-center mb-2">Response</h3>
-          <textarea value={response} readOnly className="w-full h-32 border border-gray-300 rounded px-4 py-2 focus:outline-none" />
+          <textarea
+            value={response}
+            readOnly
+            className="w-full h-32 border border-gray-300 rounded px-4 py-2 focus:outline-none text-black"
+          />
         </div>
       </div>
     </div>
